b204a719d0c7580b64dc65cfdf282b39f5dbd076
Joia <gustavo.sc.joia@gmail.com>
2024-09-04 00:57:48 -0300

    Refatoração - Implementação do Switch(Case)

--- Backend/index.php
+++ Backend/index.php
@@ -5,30 +5,53 @@ require(__DIR__.'/../vendor/autoload.php');
 $metodo = $_SERVER['REQUEST_METHOD'];
 $uri = $_SERVER['REQUEST_URI'];
 
-if($metodo==='GET' && $uri==='/Backend/index.php'){
-    http_response_code(200);
-    echo json_encode(
-        ['status'=>true, 'message'=> 'Chegou com sucesso']
-    );
-} elseif ($metodo==='POST' && '/Backend/index.php'){
-    $input = json_decode(file_get_contents('php://input'), true);
-    http_response_code(201);
-    echo json_encode(
-        ['message'=>'Usuário criado.', 'user'=>$input]
-    );
-} elseif($metodo==='PUT' && preg_match('/Backend/index.php', $uri, $matches)){
-    $id = $matches[1];
-    $input = json_decode(file_get_contents('php://input'),true);
-    $users[$id] = $input;
-    http_response_code(200);
-    echo json_encode(
-        ['status'=>true,'message'=>'Usuário atualizado.','users'=>$input]
-    );
-} elseif($metodo==='DELETE' && preg_match('/Backend/index.php',$uri, $matches)){
-    $id = $matches[1];
-    unset($users[$id]);
-    http_response_code(204);
-    echo json_encode(
-        ['status'=>true,'message'=>'Usuário deletado.']
-    );
+switch ($metodo) {
+    default:
+        http_response_code(200);
+        echo json_encode(
+            ['status' => false, 'message' => 'Método não permitido.']
+        );
+        break;
+
+    case 'GET':
+        if ($uri === '/Backend/index.php') {
+            http_response_code(201);
+            echo json_encode(
+                ['status' => true, 'message' => 'Chegou com sucesso']
+            );
+        }
+        break;
+
+    case 'POST':
+        if ($uri === '/Backend/index.php') {
+            $input = json_decode(file_get_contents('php://input'), true);
+            http_response_code(202);
+            echo json_encode(
+                ['message' => 'Usuário criado.', 'user' => $input]
+            );
+        }
+        break;
+
+    case 'PUT':
+        if (preg_match('/Backend/index.php/', $uri, $matches)) {
+            $id = $matches[1];
+            $input = json_decode(file_get_contents('php://input'), true);
+            $users[$id] = $input;
+            http_response_code(203);
+            echo json_encode(
+                ['status' => true, 'message' => 'Usuário atualizado.', 'users' => $input]
+            );
+        }
+        break;
+
+    case 'DELETE':
+        if (preg_match('/Backend/index.php/', $uri, $matches)) {
+            $id = $matches[1];
+            unset($users[$id]);
+            http_response_code(204);
+            echo json_encode(
+                ['status' => true, 'message' => 'Usuário deletado.']
+            );
+        }
+        break;
 }
